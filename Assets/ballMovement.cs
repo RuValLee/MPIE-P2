3a8a37dc920fc47ecaa834dca6dda0b564b8b762
Valerie Lee <htvl500@york.ac.uk>
2024-09-27 10:29:27 +0100

    Rolling ball game completed

---
+++ Assets/ballMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float rotationSpeed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationX = Input.GetAxis("Vertical") * rotationSpeed;
+        float rotationZ = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        transform.rotation = Quaternion.Euler(rotationX, 0, -rotationZ);
+    }
+}
